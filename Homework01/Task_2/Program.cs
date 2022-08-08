c50fe0041b6b8abb9e74c6e532baa4e40c1bd180
Darya <darya_m@74.ru>
2022-08-08 22:59:32 +0500

    add task_4

--- Homework01/Task_2/Program.cs
+++ Homework01/Task_2/Program.cs
@@ -6,24 +6,24 @@
 int a = 5;
 int b = 7;
 int max = a;
-if (a>max) max = a;
-if (b>max) max = b;
+if (a > max) max = a;
+if (b > max) max = b;
 Console.Write("max = ");
 Console.WriteLine(max);
 
 int a2 = 2;
 int b2 = 10;
 int max2 = a2;
-if (a2>max2) max2 = a2;
-if (b2>max2) max2 = b2;
+if (a2 > max2) max2 = a2;
+if (b2 > max2) max2 = b2;
 Console.Write("max = ");
 Console.WriteLine(max2);
 
 int a3 = -9;
 int b3 = -3;
 int max3 = a3;
-if (a3>max3) max3 = a3;
-if (b3>max3) max3 = b3;
+if (a3 > max3) max3 = a3;
+if (b3 > max3) max3 = b3;
 Console.Write("max = ");
 Console.WriteLine(max3);
 
---
+++ Homework01/Task_4/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+int a = 2;
+int b = 3;
+int c = 7;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write("max = ");
+Console.WriteLine(max);
+
+int a2 = 44;
+int b2 = 5;
+int c2 = 78;
+int max2 = a2;
+if (a2 > max2) max2 = a2;
+if (b2 > max2) max2 = b2;
+if (c2 > max2) max2 = c2;
+Console.Write("max = ");
+Console.WriteLine(max2);
+
+int a3 = 22;
+int b3 = 3;
+int c3 = 9;
+int max3 = a3;
+if (a3 > max3) max3 = a3;
+if (b3 > max3) max3 = b3;
+if (c3 > max3) max3 = c3;
+Console.Write("max = ");
+Console.WriteLine(max3);
+
