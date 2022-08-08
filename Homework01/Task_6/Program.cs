d973b82794541691963c42a7ae6ba0d2038bc98e
Darya <darya_m@74.ru>
2022-08-08 23:23:40 +0500

    add task_6

---
+++ Homework01/Task_6/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+int a = 4;
+if (a%2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+
+int b = -3;
+if (b%2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+
+int c = 7;
+if (c%2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
