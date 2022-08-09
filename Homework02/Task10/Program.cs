ad8f873c3fe007515e64b6ca7710b389b8e2ff93
Darya <darya_m@74.ru>
2022-08-09 22:15:24 +0500

    add task10

---
+++ Homework02/Task10/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+int a = 456;
+Console.WriteLine(a);
+Console.WriteLine((a % 100) / 10);
+
+int b = 782;
+Console.WriteLine(b);
+Console.WriteLine((b % 100) / 10);
+
+int c = 918;
+Console.WriteLine(c);
+Console.WriteLine((c % 100) / 10);
+
