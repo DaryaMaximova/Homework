ce475c58dc6afec0053a790a0a69b54ec73c2fde
Darya <darya_m@74.ru>
2022-08-13 17:46:42 +0500

    add ex02

---
+++ Lesson03/Ex01/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.CursorVisible = false;
+
+int x = 3, y = 3;
+while (true)
+{
+  var key = Console.ReadKey().Key;
+  if (key == ConsoleKey.A) { x--; }
+  if (key == ConsoleKey.D) { x++; }
+  if (key == ConsoleKey.W) { y--; }
+  if (key == ConsoleKey.S) { y++; }
+  Console.Clear();
+  Console.SetCursorPosition(x, y);
+  Console.WriteLine("+");
+}
+
---
+++ Lesson03/Ex02/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+//причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+
+//int x = 1;
+//int y = 1;
+
+int x = new Random().Next(-1000, 1000);
+int y = new Random().Next(-1000, 1000);
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+
+
+if (x > 0 & y > 0)
+{
+    Console.WriteLine("первая четверть");
+}
+if (x < 0 & y > 0)
+{
+    Console.WriteLine("вторая четверть");
+}
+if (x < 0 & y < 0)
+{
+    Console.WriteLine("третья четверть");
+}
+if (x > 0 & y < 0)
+{
+    Console.WriteLine("четвертая четверть");
+}
+if (x == 0 & y == 0)
+{
+    Console.WriteLine("начало координат");
+}
