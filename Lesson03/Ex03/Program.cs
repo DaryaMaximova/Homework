45c4a87775f2194fa478ec58d7fd3c48adc0f491
Darya <darya_m@74.ru>
2022-08-13 18:37:36 +0500

    add ex03

---
+++ Lesson03/Ex03/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая по заданному номеру четверти, 
+//показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Vvedite nomer chetverti");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n == 1)
+{
+    Console.WriteLine("x> 0 && y > 0");
+}
+if (n == 2)
+{
+    Console.WriteLine("x<0 && y > 0");
+}
+if (n == 3)
+{
+    Console.WriteLine("x<0 && y < 0");
+}
+if (n == 4)
+{
+    Console.WriteLine("x> 0 && y < 0");
+}
