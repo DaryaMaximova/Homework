31de59c9889ae829a7408558edf932079c3d75a8
Darya <darya_m@74.ru>
2022-08-13 15:51:04 +0500

    add gitignore

---
+++ Lesson02/Ex002/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая выводит случайное трехзачное число и удаляет вторую цифру этого числа. 
+//456->46
+//782->72
+//918->98
+
+
+int a = new Random().Next(100, 999);
+Console.WriteLine(a);
+int first = (a/100);
+Console.WriteLine(first);
+int last = (a/1);
+Console.WriteLine(last);
+
+Console.Write ($"{first}{last}"\n");
+
---
+++ Lesson02/Ex004/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет кратно ли оно одновременно 7 и 23
+//14->нет
+//46->нет
+//161->да
+
+Console.Write("Введите число: ");
+double value1 = Convert.ToDouble(Console.ReadLine());
+
+if (())
+
+
+
+
+if ((value1 / value2) == 0) {
+    Console.WriteLine ("Делится без остатка");
+}
+else {
+    Console.WriteLine ("Делится с остатком");
