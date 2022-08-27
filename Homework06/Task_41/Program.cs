f97f8467c39f5c2b1a4d913894bd4d21b493d6b1
Darya <darya_m@74.ru>
2022-08-27 14:56:21 +0500

    add task

---
+++ Homework06/Task_41/Program.cs
@@ -0,0 +1,30 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите количество чисел : ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[N];
+int count = 0;
+int PositiveNumbers(int N, int[] arr, int count)
+{
+    for (int i = 0; i < N; i++)
+    {
+        arr[i] = new Random().Next(-1000, 1000);
+        Console.Write("{0} ", arr[i]);
+    }
+    Console.WriteLine(" ");
+
+    foreach (int element in arr)
+    {
+        if (element > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine("Количество положительных чисел {0}", count);
+    return count;
+}
+PositiveNumbers(N, arr, count);
+
---
+++ Homework06/Task_43/Program.cs
@@ -0,0 +1,48 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int row = 2;
+int col = 2;
+
+int[,] Line = new int[row, col];
+{
+    for (int i = 0; i < Line.GetLength(0); i++)
+    {
+        for (int j = 0; j < Line.GetLength(1); j++)
+
+        {
+            Line[i, j] = new Random().Next(0, 10);
+            Console.Write($"{Line[i, j]} ");
+        }
+    }
+}
+
+
+int k1 = Line[0, 0];
+int k2 = Line[1, 0];
+int b1 = Line[0, 1];
+int b2 = Line[1, 1];
+
+Console.WriteLine("");
+Console.WriteLine($"k1= {k1} ");
+Console.WriteLine($"k2= {k2} ");
+Console.WriteLine($"b1= {b1} ");
+Console.WriteLine($"b2= {b2} ");
+
+
+if (k1 != k2)
+{
+    double x = -(b1 - b2) / (k1 - k2);
+    double y = k2 * x + b2;
+    Console.WriteLine(" ");
+    Console.WriteLine("Координаты точки пересечения прямых: " + $"{x}" + " , " + $"{y}");
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("Прямые не пересекаются");
+};
+
+Console.WriteLine();
